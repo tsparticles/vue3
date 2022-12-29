98217529cec2f027b38dd51dc7497c1bc5932512
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-12-29 18:18:52 +0100

    build: repo init

---
+++ apps/vue3/src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from "vue";
+import Particles from "@tsparticles/vue3";
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(router).use(Particles);
+
+app.mount("#app");

005f4476b7dd4af19fbd33a365bd113a50da6f8f
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-05-25 14:06:47 +0200

    build: fixed nuxt3 demo

--- apps/nuxt3/app.vue
+++ apps/nuxt3/app.vue
@@ -1,51 +1,55 @@
 <template>
-  <div>
-    <NuxtWelcome />
-    <Particles
-      id="tsparticles"
-      :options="options"
-      :particles-init="particlesInit"
-    />
-  </div>
+    <div>
+        <NuxtWelcome />
+        <Particles
+            id="tsparticles"
+            :options="options"
+            :particles-init="particlesInit"
+        />
+    </div>
 </template>
 <script>
-import { loadFull } from 'tsparticles'
+import { loadFull } from "tsparticles";
+
 export default {
-  name: 'NuxtTutorial',
-  data() {
-    return {
-      options: {
-        background: {
-          color: '#fff',
-        },
-        fullScreen: {
-          enable: true,
-          zIndex: -1,
-        },
-        particles: {
-          color: {
-            value: '#000',
-          },
-          links: {
-            color: '#000',
-            enable: true,
-          },
-          move: {
-            enable: true,
-          },
-        },
-      },
-    }
-  },
-  methods: {
-    particlesInit: async (engine) => {
-      await loadFull(engine)
+    name: "NuxtTutorial",
+    data() {
+        return {
+            options: {
+                background: {
+                    color: "#fff"
+                },
+                fullScreen: {
+                    enable: true,
+                    zIndex: -1
+                },
+                particles: {
+                    color: {
+                        value: "#000"
+                    },
+                    links: {
+                        color: "#000",
+                        enable: true
+                    },
+                    move: {
+                        enable: true
+                    },
+                    number: {
+                        value: 100
+                    }
+                }
+            }
+        };
     },
-  },
-}
+    methods: {
+        particlesInit: async (engine) => {
+            await loadFull(engine);
+        }
+    }
+};
 </script>
 <style>
 .bg-white {
-  background: none !important;
+    background: none !important;
 }
 </style>

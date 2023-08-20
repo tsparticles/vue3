7064feb638a503a0bb158c49d0a76d16142bede2
Joey Pereira <contact@joeypereira.dev>
2023-08-20 19:13:47 -0400

    fix(example): clean up Vite example and remove dead code

--- apps/vue3/src/main.ts
+++ apps/vue3/src/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from "vue";
 import Particles from "vue3-particles";
 import App from "./App.vue";
-import router from "./router";
 
 import "./assets/main.css";
 
 const app = createApp(App);
 
-app.use(router).use(Particles);
+app.use(Particles);
 
 app.mount("#app");

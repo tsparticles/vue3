b9d95f3616fe21d1ca760002aeabdf0394a9a2c3
Joey Pereira <contact@joeypereira.dev>
2023-08-25 14:18:11 -0400

    feat(example): Add Nuxt 2 example

---
+++ apps/nuxt2/nuxt.config.js
@@ -0,0 +1,36 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Nuxt 2 tsParticles Demo',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+  },
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    '@nuxtjs/composition-api/module'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: ['@tsparticles/engine', 'tsparticles']
+  },
+
+  ssr: false
+}
---
+++ apps/nuxt2/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Particles
+      id="tsparticles"
+      :options="particlesConfig"
+      :init="initParticles"
+      @load="onParticlesLoaded"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Particles } from '@tsparticles/vue/dist'
+import type { Engine, Container } from '@tsparticles/engine'
+import { loadFull } from 'tsparticles'
+import { particlesConfig } from '~/lib/particles'
+
+const initParticles = async (engine: Engine) => {
+  await loadFull(engine)
+}
+
+const onParticlesLoaded = async (container: Container) => {
+  console.log(container)
+}
+</script>
